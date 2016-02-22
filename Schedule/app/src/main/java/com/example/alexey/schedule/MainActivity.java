30edec4f1833ad2f7f8dd50e33f8fec1030dad94
aLT22 <altsworks@gmail.com>
2016-02-22 22:23:15 +0300

    22.02.16 - 2nd commit
    добавил контекстное меню(недоделанное)
    сделал пару улучшений по коду
    обозвал все вью нормальными именами и добавил им id

--- Schedule/app/src/main/java/com/example/alexey/schedule/MainActivity.java
+++ Schedule/app/src/main/java/com/example/alexey/schedule/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.alexey.schedule;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,10 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         spinner = (Spinner) findViewById(R.id.spinner);
         listView = (ListView) findViewById(R.id.listView);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        registerForContextMenu(spinner);
+        registerForContextMenu(listView);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
                     case 0:
 
                         break;
@@ -51,6 +53,11 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 
@@ -69,4 +76,29 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu_spinner, menu);
+        inflater.inflate(R.menu.context_menu_list, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.rewrite:
+
+                return true;
+            case R.id.add:
+
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
 }

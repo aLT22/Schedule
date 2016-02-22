e12f0574eed972adede9f012526dbc4368aba9fc
aLT22 <altsworks@gmail.com>
2016-02-22 17:01:28 +0300

    22.02.16
    создал проект и основные макеты и элементы

---
+++ Schedule/app/src/main/java/com/example/alexey/schedule/AddInformation.java
@@ -0,0 +1,13 @@
+package com.example.alexey.schedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_information);
+    }
+}
---
+++ Schedule/app/src/main/java/com/example/alexey/schedule/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.alexey.schedule;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        listView = (ListView) findViewById(R.id.listView);
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.add_timetable) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

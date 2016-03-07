44ca5acd234b605d235b561cb21ea942f9620740
aLT22 <altsworks@gmail.com>
2016-03-07 22:36:01 +0300

    7.03.16 22:35
    Создан основной каркас из классов и активностей для приложения,
    подготовка к заполнению вью-элементами активностей приложения

---
+++ Schedule/app/src/main/java/com/example/alexey/schedule/MiddleLevelTwo.java
@@ -0,0 +1,13 @@
+package com.example.alexey.schedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiddleLevelTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_middle_level_two);
+    }
+}

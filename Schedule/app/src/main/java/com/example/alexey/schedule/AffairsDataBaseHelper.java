a829c971f3d24a52b769e96ccd4475388f7ac782
aLT22 <altsworks@gmail.com>
2016-03-27 21:59:41 +0300

    Added SQLite, Some fonts and colors etc
    added supporting SQLite
    added activity BLA - bottom level activity three
    added database
    added categories in table "category" in db
    added some little fixes in activities' interfaces

---
+++ Schedule/app/src/main/java/com/example/alexey/schedule/AffairsDataBaseHelper.java
@@ -0,0 +1,50 @@
+package com.example.alexey.schedule;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static android.provider.Settings.Global.getString;
+
+/**
+ * Created by Alexey on 27.03.2016.
+ */
+public class AffairsDataBaseHelper extends SQLiteOpenHelper {
+    public static final String DB_NAME = "affairs";
+    public static final int DB_VERSION = 1;
+
+    public AffairsDataBaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE CATEGORY ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT NOT NULL);");
+        fillingDB("CATEGORY", "NAME", "Бизнес", db);
+        fillingDB("CATEGORY", "NAME", "Спорт", db);
+        fillingDB("CATEGORY", "NAME", "Встреча", db);
+        fillingDB("CATEGORY", "NAME", "Учеба", db);
+        fillingDB("CATEGORY", "NAME", "Работа", db);
+        fillingDB("CATEGORY", "NAME", "Свободное время", db);
+        db.execSQL("CREATE TABLE AFFAIR ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT NOT NULL, "
+                + "DESCRIPTION TEXT, "
+                + "DATE TEXT, "
+                + "CATGRY INTEGER NOT NULL, "
+                + "FOREIGN KEY (CATGRY) REFERENCES CATEGORY (_id));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+
+    private void fillingDB(String dbName, String coloumnName, String sValue, SQLiteDatabase db){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(coloumnName, sValue);
+        db.insert(dbName, null, contentValues);
+    }
+}
--- Schedule/app/src/main/java/com/example/alexey/schedule/MiddleLevelOne.java
+++ Schedule/app/src/main/java/com/example/alexey/schedule/MiddleLevelOne.java
@@ -1,21 +1,58 @@
 package com.example.alexey.schedule;
 
-import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 
 public class MiddleLevelOne extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_middle_level_one);
+        ListView listView = (ListView) findViewById(R.id.slCategoriesListView);
+        try{
+            SQLiteOpenHelper affairsDataBaseHelper = new AffairsDataBaseHelper(this);
+            db = affairsDataBaseHelper.getReadableDatabase();
+            cursor = db.query("CATEGORY",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1, cursor,
+                    new String[]{"NAME"}, new int[]{android.R.id.text1}, 0);
+            listView.setAdapter(listAdapter);
+        } catch (SQLiteException ex){
+            Toast toast = Toast.makeText(this, "SQLITE EXCEPTION", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     public void onAddCategoryClick(View view){
-        Intent intent = new Intent(this, MiddleLevelTwo.class);
+        Intent intent = new Intent(this, BottomLevelThree.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cursor != null){
+            cursor.close();
+        }
+        if (db != null){
+            db.close();
+        }
     }
 }

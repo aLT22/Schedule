4a7adfc3c828da1f50857c46118a9d6d020fd288
aLT22 <altsworks@gmail.com>
2016-03-28 18:34:53 +0300

    Added function which apply users to add their own categories
    added onClickListener for adding own category

--- Schedule/app/src/main/java/com/example/alexey/schedule/BottomLevelThree.java
+++ Schedule/app/src/main/java/com/example/alexey/schedule/BottomLevelThree.java
@@ -1,7 +1,13 @@
 package com.example.alexey.schedule;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BottomLevelThree extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class BottomLevelThree extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_level_three);
+    }
+
+    public void onAddConcreteCategoryClick(View view){
+        EditText writeCategoryName = (EditText) findViewById(R.id.writeCategoryName);
+        String categoryName = writeCategoryName.getText().toString();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NAME", categoryName);
+        SQLiteOpenHelper affairsDataBaseHelper = new AffairsDataBaseHelper(this);
+        SQLiteDatabase db = affairsDataBaseHelper.getWritableDatabase();
+        db.insert("CATEGORY", null, contentValues);
     }
 }
